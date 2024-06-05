690b026b8efe7e3cc8ee943a5b4c954832b4c3cc
Nektarios_Tsagkaris <nektarios.tsagkaris@gmail.com>
2024-06-05 12:58:27 +0300

    DicerollProject

---
+++ DiceRollGame/Game/GuessingGame.cs
@@ -0,0 +1,54 @@
+﻿
+using DiceRollGame.Game.UserCommunication;
+
+namespace DiceRollGame.Game
+{
+    public class GuessingGame
+    {
+        private readonly Dice _dice;
+        private const int InitialTries = 3;
+
+        public GuessingGame(Dice dice)
+        {
+            _dice = dice;
+        }
+
+        public GameResult Play()
+        {
+            var diceRollResult = _dice.Roll();
+            Console.WriteLine(
+                $"Dice rolled.Guess what number it shows in {InitialTries} tries.");
+
+            var triesLeft = InitialTries;
+            while (triesLeft > 0)
+            {
+                var guess = ConsoleReader.ReadInteger("Enter a number:");
+                if (guess == diceRollResult)
+                {
+                    return GameResult.Victory;
+                }
+
+                Console.WriteLine("Wrong number");
+                --triesLeft;
+            }
+            return GameResult.Loss;
+        }
+
+        public static void PrintResult(GameResult gameResult)
+        {
+            //string message;                  KANONIKOS TROPOS
+            //if (gameResult == GameResult.Victory)
+            //{
+            //    message = ("You win!");
+            //}
+            //else
+            //{
+            //    message = ("You lose :(");
+            //}
+            //Console.WriteLine(message);
+
+            string message = gameResult == GameResult.Victory ? "You win!" : "You lose:(";
+            Console.WriteLine(message);//TERNARY Condiotional oparetor (apofeygoume olo to if else statment kai to kanoume se mia grammh)
+        }
+    }
+}
---
+++ DiceRollGame/Program.cs
@@ -0,0 +1,50 @@
+﻿
+//var diceRollResult = random.Next(1,7);
+//Console.WriteLine(diceRollResult);
+//for (int i = 0; i < 3; i++)
+//{
+//    Console.WriteLine(random.Next(1, 11));
+//}
+
+//public enum Season
+//{
+//    Spring,
+//    Summer,
+//    Autumn,
+//    Winter
+//}
+
+//Season firstSeason = Season.Spring;
+//Season lastSeason = Season.Winter;
+//int firstSeasonNumber = (int)firstSeason;
+//int lastSeasonNumber = (int)lastSeason;
+
+//Console.WriteLine(firstSeason);
+//Console.WriteLine("As a number " + firstSeasonNumber);
+//Console.WriteLine(lastSeason);
+//Console.WriteLine("As a number " + lastSeasonNumber);How the enums works
+
+
+
+using DiceRollGame.Game;
+
+var random = new Random();
+var dice = new Dice(random);
+var guessingGame = new GuessingGame(dice);
+GameResult gameResult = guessingGame.Play();
+GuessingGame.PrintResult(gameResult);
+
+//if (gameResult == GameResult.Victory)
+//{
+//    Console.WriteLine("You win!");
+//}
+//else
+//{
+//    Console.WriteLine("You lose :("); swstos tropos
+//}
+
+
+Console.ReadKey();
+
+
+ 
